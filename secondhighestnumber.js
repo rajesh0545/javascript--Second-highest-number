54f139e30ae64b2f373d5b945e3152778214af68
rajesh <krajeshgowri7032@mail.com>
2022-10-03 07:38:50 +0530

    add files

---
+++ secondhighestnumber.js
@@ -0,0 +1,14 @@
+// const number = [2125,343,2,10,218,80,4];
+
+// console.log(number.sort((a,b)=>a-b));
+
+// console.log("the 2nd largest number is"+number[number.length-2])
+
+// console.log(number.length);
+
+
+let selectedColors = ["red", "green", "yellow",5,6,77,88]
+
+// selectedColors[3]="pink";
+
+console.log(selectedColors);
